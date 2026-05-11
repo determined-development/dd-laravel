cf3b305ce9d2b0661cff9b307cea3c5966de71ca
Samuel Levy <sam@determineddevelopment.com>
2026-05-11 15:52:15 +1000

    Initial cleanups for skeleton

--- config/database.php
+++ config/database.php
@@ -178,7 +178,7 @@ return [
                 'prefix' => '',
                 'prefix_indexes' => true,
             ],
-            default =>[
+            default => [
                 'driver' => 'sqlite',
                 'url' => env('DB_URL'),
                 'database' => ':memory:',
@@ -224,7 +224,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
